150e0f644bd0a409beba6d30fe6205d0c39f41fd
Rafael Van Belle <rafael@dataroots.io>
2023-05-03 15:49:31 +0200

    first commit

--- src/model/train.py
+++ src/model/train.py
@@ -34,7 +34,10 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df)
 
+    y = df.iloc[:, -1]
+    X = df[~y]
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
